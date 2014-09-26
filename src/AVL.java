caa6c9575a7e37964799c1936be3bbc51d9d3b17
Somatrix <rkev91@gmail.com>
2014-09-26 15:53:39 +0400

    hazpifhzepifhzipufhepizhf

--- src/AVL.java
+++ src/AVL.java
@@ -195,90 +195,103 @@ class AVL<T extends Comparable<T>>{
 		}		
 	}
 	
+	//Principe : On recherche d'abord l'arbre de l'élément à supprimer dans l'arbre. Si l'élément à supprimer est une feuille on la supprime.
+	// Si l'arbre possède un fils, on le remplace par son fils. Si l'arbre possède 2 fils, on prend le plus petit élément de son sous arbre droit
+	// et on le place à la place de la racine de l'élément à supprimer, en pensant bien à recoller tout les noeuds de l'arbre. 
+	// On recalcule ensuite le déséquilibre de l'arbre et on applique son rééquilibrage au besoin.
+	//Entrée : un arbre AVL, et un élément 
+	//Sortie : l'arbre AVL avec une occurence de l'élément enlevé de l'arbre.
 	public void suppressionAVL(T element){
-		//recherche de element
+		//on recherche l'element à supprimer
 		AVL eltrouve= this.rechercheNoeud(element);
-		
+			
+			
+		// Si l'element recherché est une feuille on la supprime
 		if(estFeuille(eltrouve)){
-			//on supprime
+				
 			eltrouve.parent = null;
 			eltrouve.valeur = null;
 			eltrouve.filsG = null;
 			eltrouve.filsD = null;
-			
-			
+				
+				
 		}
-		
+			
 		else if(eltrouve.filsG==null){
-			//si x n'a qu'un fils, on le remplace par son fils
+			//si x n'a qu'un fils (ici l'arbre gauche vide), on le remplace par son fils
 			AVL tempo = new AVL(eltrouve.parent, (T)eltrouve.filsD.valeur, eltrouve.filsD.filsG, eltrouve.filsD.filsD);
 			eltrouve.parent = eltrouve.parent;
 			eltrouve.valeur = (T)eltrouve.filsD.valeur;
 			eltrouve.filsG = eltrouve.filsD.filsG;
 			eltrouve.filsD = eltrouve.filsD.filsG;
-			
-			
+				
+				
 		}
 		else if(eltrouve.filsD==null){
-			//si x n'a qu'un fils, on le remplace par son fils
+			//si x n'a qu'un fils (ici l'arbre droit vide), on le remplace par son fils
 			AVL tempo = new AVL(eltrouve.parent, (T)eltrouve.filsG.valeur, eltrouve.filsG.filsG, eltrouve.filsG.filsD);
 			eltrouve.parent = tempo.parent;
 			eltrouve.valeur = (T)tempo.valeur;
 			eltrouve.filsG = tempo.filsG;
 			eltrouve.filsD = tempo.filsD;
-			
+				
 		}
 		else{
 			//si x a 2 fils, on le remplace par le plus petit element de son sous arbre droit
-			AVL tempo2 = eltrouve.filsD;
+			AVL tempo2 = eltrouve.filsD; // parcours vers le coté le plus à gauche du sous arbre droit de l'arbre de l'element a supprimer.
 			while(!estFeuille(tempo2)){
-				tempo2 = tempo2.filsG;
+				tempo2 = tempo2.filsG; 
 			}
-
+			//On modifie l'arbre avec le plus petit element du sous arbre droit placé a la racine de l'element supprimé.
 			AVL tempo = new AVL(eltrouve.parent, tempo2.valeur, eltrouve.filsG, eltrouve.filsD);
-			
-			
+				
+				
 			eltrouve.parent = tempo.parent;
 			eltrouve.valeur = (T)tempo.valeur;
 			eltrouve.filsG = tempo.filsG;
 			eltrouve.filsD = tempo.filsD;
 			tempo2 = tempo2.filsD; // On recolle l'arbre de racine eltrouve avec l'arbre de l'element remplacé 	
-			
+				
 		}
-		
 		parcoursDesequilibre(this); //calcul des desequilibres
 		
 		//rééquilibrage si necessaire
 		this.equilibrage();
 	}
 	
+	// Principe : On regarde si la racine de l'arbre est feuille, et si il l'est on renvoie l'arbre sans rien toucher.
+	// sinon si la racine a un déséquilibre différent de zero, on regarde si la racine et déséquilibré et on applique les 
+	// rotations dans ce cas. Tant qu'on a pas atteint tout l'arbre, la fonction se poursuit récursivement dans les sous arbres. On renvoie l'arbre équilibré a la fin.
+	//Entrée : un arbre AVL non vide
+	//Sortie : l'arbre AVL équilibré
 	public AVL equilibrage() {
 		AVL tmp = null;
-		if (estFeuille(tmp)){
+		if (estFeuille(tmp)){ // si l'arbre est une feuille pas besoin de rééquilibrage, on renvoir l'arbre intact
 			tmp = this;
 		}
 		
-		else {
+		// sinon on verifie le déséquilibre de la racine de l'arbre
+		else { 
 			tmp = this;
 			int desequilibre = tmp.deseq;
-			if (desequilibre >=1){
+			if (desequilibre >=1){ //Si on a un déséquilibre positif sur la racine suivi de son sous arbre gauche négatif l'arbre est déséquilibré 
 				if (tmp.filsG != null) {
-					if (tmp.filsG.deseq<0){
+					if (tmp.filsG.deseq<0){  //on applique donc la rotation approprié : ici, la rotation gauche droite
 						tmp.filsG = tmp.filsG.rotationG();
 					tmp = tmp.rotationD();
 					}
 				}
 			}
-			if (desequilibre <=1){
+			if (desequilibre <=1){ //Si on a un déséquilibre négatif sur la racine suivi de son sous arbre gauche positif l'arbre est déséquilibré 
 				if (tmp.filsD != null){
-					if (tmp.filsD.deseq >0){
+					if (tmp.filsD.deseq >0){  //on applique donc la rotation approprié : ici, la rotation droite gauche
 						tmp.filsD = tmp.filsD.rotationD();
 					}
 					tmp = tmp.rotationG();
 				}
 			}
-			
-			if (tmp.filsG != null){
+			// tant qu'on a pas atteint le bout de l'arbre, on applique une récursivité de la fonction sur ses 2 sous arbres
+			if (tmp.filsG != null){  
 				tmp.filsG = tmp.filsG.equilibrage();
 			}
 			if (tmp.filsD != null) {
